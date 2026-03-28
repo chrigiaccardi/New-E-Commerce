5dad4955ac2ea39f0bac64a79e0d478e4ce3f8aa
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac-2.lan>
2026-03-28 17:00:04 +0100

    Conclusione E Commerce con integrazione stripe per pagamento e inserimento spedizione

--- server/server.js
+++ server/server.js
@@ -12,9 +12,56 @@ app.use(cors({ origin: true, credentials: true }));
 
 //INSERIRE QUA API STRIPE
 
+
+
 app.post('/checkout', async (req, res, next) => {
     try {
         const session = await stripe.checkout.sessions.create({
+            shipping_address_collection: {
+                allowed_countries: ['IT'],
+            },
+            shipping_options: [
+                {
+                shipping_rate_data: {
+                    type: 'fixed_amount',
+                    fixed_amount: {
+                        amount: 0,
+                        currency: 'EUR',
+                    },
+                    display_name: 'Spedizione Gratuita',
+                    delivery_estimate: {
+                        minimum: {
+                            unit: 'business_day',
+                            value: 5,
+                        },
+                        maximum: {
+                            unit: 'business_day',
+                            value: 7,
+                            },
+                        },
+                    },
+                },
+                {
+                shipping_rate_data: {
+                    type: 'fixed_amount',
+                    fixed_amount: {
+                        amount: 1500,
+                        currency: 'EUR',
+                        },
+                    display_name: 'Giorno Successivo',
+                    delivery_estimate: {
+                        minimum: {
+                            unit: 'business_day',
+                            value: 1,
+                        },
+                        maximum: {
+                            unit: 'business_day',
+                            value: 1,
+                        },
+                    },
+                },
+            },
+        ],
             line_items: req.body.items.map((item) => ({
                 price_data: {
                     currency: "EUR",
